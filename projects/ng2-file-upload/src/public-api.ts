a4cede0e2f3300f93271b2840ddee60e7192732c
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-09-25 19:23:19 +0200

    Update to use angular cli

---
+++ projects/ng2-file-upload/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of ng2-file-upload
+ */
+
+
+export * from './lib/file-upload.module';
+export * from './lib/file-drop.directive';
+export * from './lib/file-item.class';
+export * from './lib/file-like-object.class';
+export * from './lib/file-select.directive';
+export * from './lib/file-type.class';
+export * from './lib/file-uploader.class';
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+// const gettingStarted = require('html-loader!markdown-loader!../getting-started.md');
+
+@Component({
+  selector: 'app',
+  template: ``
+})
+export class AppComponent {
+  // public gettingStarted: string = gettingStarted;
+  // public ngAfterContentInit(): any {
+  //   setTimeout(() => {
+  //     if (typeof PR !== 'undefined') {
+  //       // google code-prettify
+  //       PR.prettyPrint();
+  //     }
+  //   }, 150);
+  // }
+}
